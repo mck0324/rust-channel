a29196bfbfcf4ae25fac4859b6bddf16f5d3d2bb
mck0324 <dch3030@naver.com>
2024-10-25 01:07:53 +0900

    Tokio::mpsc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::sync::mpsc;
+
+#[tokio::main]
+async fn main() {
+    let (tx,mut rx) = mpsc::channel(32);
+
+    tokio::spawn(async move {
+        tx.send("Hello").await.unwrap();
+    });
+
+    while let Some(msg) = rx.recv().await {
+        println!("Recived: {}",msg);
+    }
 }
