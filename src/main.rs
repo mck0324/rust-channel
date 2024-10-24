4203955e77c8ba8f4aae809c3bdd05a85d85a583
mck0324 <dch3030@naver.com>
2024-10-25 01:12:39 +0900

    BroadCast

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx1) = broadcast::channel(16);
+    let mut rx2 = tx.subscribe();
+
+    tokio::spawn(async move {
+        tx.send("Broad Msg").unwrap();
+    });
+    tokio::spawn(async move {
+        if let Ok(msg) = rx1.recv().await {
+            println!("Recv 1: {}",msg);
+        }
+    });
+    if let Ok(msg) = rx2.recv().await {
+        println!("Recv 2: {}",msg);
+    }
 }
