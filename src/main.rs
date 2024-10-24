f54a613bcc4846c6c31fea042f02dbdcae4d0bdb
mck0324 <dch3030@naver.com>
2024-10-25 01:44:03 +0900

    Select

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::time::Duration;
+
+use tokio::time::sleep;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    tokio::select! {
+        _ = sleep(Duration::from_secs(5)) => {
+            println!("5초후 타이머 완료");
+        },
+
+        _ = my_async_task() => {
+            println!("비동기 작업 완료");
+        }
+    }
+}
+
+async fn my_async_task() {
+    sleep(Duration::from_secs(3)).await;
+    println!("3초 비동기 작업 완료");
 }
