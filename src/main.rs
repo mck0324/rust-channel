28d207d0b8aba44e6df85a87a69e49ca5751a131
mck0324 <dch3030@naver.com>
2024-10-25 01:01:40 +0900

    Tokio::Oneshot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::sync::oneshot;
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = oneshot::channel();
+
+    tokio::spawn(async move {
+        tx.send("완료").unwrap();
+    });
+
+    let result = rx.await;
+    println!("받은 값: {:?}", result);
 }
