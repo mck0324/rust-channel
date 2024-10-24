f7b7a759c48cdcdc050c2b47c319e5d7417b1a8c
mck0324 <dch3030@naver.com>
2024-10-25 01:25:46 +0900

    spawn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::time::Duration;
+
+use tokio::time::sleep;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let handle = tokio::spawn(async {
+        println!("비동기 태스크 시작");
+        sleep(Duration::from_secs(2)).await;
+        println!("비동기 태스크 완료");
+    });
+    println!("메인테스크 실행중");
+    handle.await.unwrap();
 }
